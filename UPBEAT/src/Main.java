1c08bf3c5c58a2dd4b242ccdfc27a5790e8239a6
guita13921 <guita13921@gmail.com>
2023-02-18 13:27:39 +0700

    add Tokenizer Test01

--- UPBEAT/src/Main.java
+++ UPBEAT/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         Tokenizer token = new Tokenizer("t = t + 1");
         System.out.println(token.show_token());
         System.out.println(token.consume());
@@ -7,5 +8,20 @@ public class Main {
         System.out.println(token.consume());
         System.out.println(token.consume());
         System.out.println(token.consume());
+
+        Tokenizer token1 = new Tokenizer("while (deposit)");
+        System.out.println(token1.consume());
+        System.out.println(token1.consume());
+        System.out.println(token1.consume());
+
+        Tokenizer token2 = new Tokenizer("if (deposit - 100)");
+        System.out.println(token2.consume());
+        System.out.println(token2.consume());
+        System.out.println(token2.consume());
+        System.out.println(token2.consume());
+        System.out.println(token2.consume());
+        System.out.println(token2.consume());
+
+
     }
 }
