910072d71e93af2a7bf5cfb0d6729de126d26fc5
bewzunjo <saran_kim@cmu.ac.th>
2023-02-26 15:22:04 +0700

    p

--- UPBEAT/src/Mapp.java
+++ UPBEAT/src/Mapp.java
@@ -1,19 +1,34 @@
 import java.util.ArrayList;
 
 public class Mapp {
-  private  int m;
-  private  int n;
-  ArrayList<Box>map = new ArrayList<>();
-    public void MappArea(int n,int m){
-      this.n = n;
-      this.m = m;
-    }
-    public void GenerateMap(){
-      for(int i = 1;i<n;i++){
-        for(int j = 1;j<m;j++){
-          map.add(new Box(n,m));
+  private int Budget = 0;
+  private int m;
+  private int n;
+  ArrayList<Box> map = new ArrayList<>();
 
-        }
+  public void MappArea(int n, int m) {
+    this.n = n;
+    this.m = m;
+  }
+
+  public void GenerateMap() {
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        map.add(new Box(n, m));
+
       }
     }
+  }
+  public void own(){
+  }
+  public void Invest(int invest) {
+    Budget = Budget+invest;
+    if(Budget>0) {
+      own();
+    }else if(Budget == 0){
+      own();
+    }if(Budget <0){
+      own();
+    }
+  }
 }
--- UPBEAT/src/Player.java
+++ UPBEAT/src/Player.java
@@ -1,7 +1,8 @@
+import java.util.LinkedList;
+
 public class Player {
     int budget;
     Box citycenter;
     Box location;
-
-
+    LinkedList<Mapp> Ownerme = new LinkedList<>();
 }
