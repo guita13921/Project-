7c1ab4ca1e6a157bd8faa626931934116c38e88a
guita13921 <guita13921@gmail.com>
2023-03-06 14:56:19 +0700

    add Mapp

--- UPBEAT/src/Main.java
+++ UPBEAT/src/Main.java
@@ -1,11 +1,9 @@
-import Parser.Parser;
 
 public class Main {
     public static void main(String[] args) {
+        Mapp demo = new Mapp(5,5);
+        System.out.println(demo.ShowMap()+"\n");
+        demo.MapPrettyPrint();
+
     }
-    /*
-            Reader read = new Reader("Sample construction plan.txt");
-            read.readFile();
-            Configuration_Reader conRead = new Configuration_Reader("");
-    */
 }
--- UPBEAT/src/Mapp.java
+++ UPBEAT/src/Mapp.java
@@ -2,21 +2,34 @@ import java.util.ArrayList;
 
 public class Mapp {
   protected int Budget = 0;
-  protected int m;
-  protected int n;
-  protected
-  ArrayList<Box> map = new ArrayList<>();
+  private int rows;
+  private int cols;
+  protected ArrayList<Box> map = new ArrayList<>();
 
-  public void MappArea(int n, int m) {
-    this.n = n;
-    this.m = m;
+  public Mapp(int rows, int cols) {
+    this.rows = rows;
+    this.cols = cols;
+    GenerateMap();
   }
 
   public void GenerateMap() {
-    for (int i = 1; i < n; i++) {
-      for (int j = 1; j < m; j++) {
-        map.add(new Box(n, m));
+    for (int i = 1; i < rows+1; i++) {
+      for (int j = 1; j < cols+1; j++) {
+        map.add(new Box(i, j));
+      }
+    }
+  }
+  public ArrayList<Box> ShowMap(){
+   return  map;
+  }
 
+  public void MapPrettyPrint(){
+    int count = 0;
+    for(int i = 0;i<map.size();i++){
+        System.out.print(map.get(i).box_row_show() + "," + map.get(i).box_column_show()+" ");
+      count++;
+      if (count % rows == 0) {
+        System.out.println(" ");
       }
     }
   }
